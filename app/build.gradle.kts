bcedacd29a514d531151476c1624c747732e7cc5
harsha-acc <balasaharsha@gmail.com>
2025-01-09 11:35:16 +0530

    inc compileSdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.hb.daggersampleapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hb.daggersampleapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
