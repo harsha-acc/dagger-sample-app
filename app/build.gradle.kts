2731b655a6fa74242d99f3d1e3c76965c7404d14
harsha-acc <balasaharsha@gmail.com>
2025-01-09 11:31:07 +0530

    add dagger2 framework dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
 
 android {
@@ -36,6 +37,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
 }
 
@@ -56,4 +58,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    /**
+     * dagger related dependencies
+     */
+    dependencies {
+        implementation(libs.dagger)
+        kapt(libs.dagger.compiler)
+    }
 }
